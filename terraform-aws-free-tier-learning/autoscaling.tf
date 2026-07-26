bfc84aaeeae5e0adf3108474a832b83ff2cdad6a
Gowri855 <gowriM@08052005@gmail.com>
2026-07-26 11:33:43 +0530

    public alb

--- terraform-aws-free-tier-learning/autoscaling.tf
+++ terraform-aws-free-tier-learning/autoscaling.tf
@@ -3,7 +3,7 @@ resource "aws_autoscaling_group" "app" {
   min_size            = var.min_size
   desired_capacity    = var.desired_capacity
   max_size            = var.max_size
-  vpc_zone_identifier = [aws_subnet.private_1.id]
+  vpc_zone_identifier = [aws_subnet.private_1.id ,aws_subnet.private_2]
   target_group_arns   = [aws_lb_target_group.app.arn]
   health_check_type   = "ELB"
 
