aa6cac6e985ed701ae4985fa6147c1f33e22a69c
Gowri855 <gowriM@08052005@gmail.com>
2026-07-26 12:14:32 +0530

    public alb

--- terraform-aws-free-tier-learning/variables.tf
+++ terraform-aws-free-tier-learning/variables.tf
@@ -146,9 +146,3 @@ variable "cloudfront_price_class" {
   type        = string
   default     = "PriceClass_100"
 }
-
-variable "application_port" {
-  description = "Port used by the application"
-  type        = number
-  default     = 80
-}
