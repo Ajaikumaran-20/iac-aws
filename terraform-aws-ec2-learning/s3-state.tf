20bb8662a33da774086b6abb3b83a98c5a231aca
Gowri855 <gowriM@08052005@gmail.com>
2026-09-05 11:18:45 +0530

    Fix Terraform state bucket name

--- terraform-aws-ec2-learning/s3-state.tf
+++ terraform-aws-ec2-learning/s3-state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "playgroung-614939597046-ap-south-1-an"
+    bucket  = "playground-614939597046-ap-south-1-an"
     key     = "terraform/ec2/state.tfstate"
     region  = "ap-south-1"
     encrypt = true
--- terraform-aws-free-tier-learning/s3-state.tf
+++ terraform-aws-free-tier-learning/s3-state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "playgroung-614939597046-ap-south-1-an"
+    bucket  = "playground-614939597046-ap-south-1-an"
     key     = "terraform/state.tfstate"   # path inside the bucket
     region  = "ap-south-1"
     encrypt = true
